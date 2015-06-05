5e027290c4d56bbc22c5692c3b5eaeb48ddcd878
Kenneth Crawford <kmc059000@gmail.com>
2015-06-05 09:59:53 -0500

    giphy command

---
+++ XmppBot.Plugins/Giphy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+using Newtonsoft.Json;
+
+using XmppBot.Common;
+
+namespace XmppBot.Plugins
+{
+    [Export(typeof(IXmppBotPlugin))]
+    public class Giphy : XmppBotPluginBase, IXmppBotPlugin
+    {
+        public override string EvaluateEx(ParsedLine line)
+        {
+            if (!line.IsCommand || line.Command.ToLower() != "giphy") return null;
+
+            var args = string.Join(" ", line.Args);
+
+
+            Task.Factory.StartNew(async () =>
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(new Uri("http://api.giphy.com/v1/gifs/search?q=" + HttpUtility.UrlEncode(args) + "&api_key=dc6zaTOxFJmzC"));
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var results = JsonConvert.DeserializeObject<GiphyDatas>(await response.Content.ReadAsStringAsync());
+
+                        var data = results != null && results.data != null ? results.data.FirstOrDefault() : null;
+
+                        if (data != null && data.images != null && data.images.original != null)
+                        {
+                            this.SendMessage(data.images.original.url, line.From, BotMessageType.groupchat);
+                        }
+                    }
+                }
+            });
+
+            return null;
+        }
+
+        public override string Name
+        {
+            get { return "User Actions"; }
+        }
+
+        private class GiphyDatas
+        {
+            public IEnumerable<GiphyData> data { get; set; }
+
+            public class GiphyData
+            {
+                public GiphyImages images { get; set; }
+
+                public class GiphyImages
+                {
+                    public Image original { get; set; }
+
+                    public class Image
+                    {
+                        public string url { get; set; }
+                    }
+                }
+            }
+        }
+    }
+}
