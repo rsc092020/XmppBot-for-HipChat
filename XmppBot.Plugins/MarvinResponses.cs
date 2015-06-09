a59d8e819ffff0b37abc98943ec5a759a41f0cd5
Kenneth Crawford <kmc059000@gmail.com>
2015-06-09 17:00:31 -0500

    marvin responses

---
+++ XmppBot.Plugins/MarvinResponses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Text.RegularExpressions;
+
+using XmppBot.Common;
+
+namespace XmppBot.Plugins
+{
+    [Export(typeof(IXmppBotPlugin))]
+    public class MarvinResponses : XmppBotPluginBase, IXmppBotPlugin
+    {
+        private static readonly Random _random = new Random();
+        private static readonly Regex _shutUpRegex = new Regex("shut\\s*up\\s*marvin", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1));
+
+        public override string EvaluateEx(ParsedLine line)
+        {
+            if (_shutUpRegex.IsMatch(line.Raw))
+            {
+                switch (_random.Next(4))
+                {
+                    case 0: return "why do i even bother";
+                    case 1: return "im so depressed";
+                    case 2: return "(areyoukiddingme)";
+                    case 3: return "(ohgodwhy)";
+                }
+            }
+
+            return null;
+        }
+
+        public override string Name
+        {
+            get { return "Marvin"; }
+        }
+    }
+}
