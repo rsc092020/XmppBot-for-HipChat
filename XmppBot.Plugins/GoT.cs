b4261c75f97ca7fdefeec8431bcddc797a2f4442
Kenneth Crawford <kmc059000@gmail.com>
2015-06-09 16:57:51 -0500

    GoT

---
+++ XmppBot.Plugins/GoT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Text.RegularExpressions;
+
+using XmppBot.Common;
+
+namespace XmppBot.Plugins
+{
+    [Export(typeof(IXmppBotPlugin))]
+    public class GoT : XmppBotPluginBase, IXmppBotPlugin
+    {
+        private static readonly Random _random = new Random();
+        private static readonly Regex _regex1 = new Regex("GoT", RegexOptions.None, TimeSpan.FromSeconds(1));
+        private static readonly Regex _regex2 = new Regex("game\\s*of\\s*thrones", RegexOptions.IgnoreCase, TimeSpan.FromSeconds(1));
+
+        public override string EvaluateEx(ParsedLine line)
+        {
+            if (_regex1.IsMatch(line.Raw) || _regex2.IsMatch(line.Raw))
+            {
+                switch (_random.Next(6))
+                {
+                    case 0: return "(arya)";
+                    case 1: return "(jonsnow)";
+                    case 2: return "(joffrey)";
+                    case 3: return "(daenerys)";
+                    case 4: return "(tyrion)";
+                    case 5: return "(tywin)";
+                }
+            }
+
+            return null;
+        }
+
+        public override string Name
+        {
+            get { return "ChrisSurprises"; }
+        }
+    }
+}
